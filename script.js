59505daa1a78776e3a5d92eb4db1f90933426324
jinnajhanvi <jinnajhanvi@gmail.com>
2024-06-13 06:50:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+console.log("Welcome to Tic Tac Toe");
+let music = new Audio("music.mp3");
+let audioTurn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "X";
+let isgameover = false;
+
+const changeTurn = () => {
+    return turn === "X" ? "O" : "X";
+}
+
+const checkWin = () => {
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    wins.forEach(e => {
+        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && 
+            (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && 
+            (boxtext[e[0]].innerText !== "")) {
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won";
+            isgameover = true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            showModal(boxtext[e[0]].innerText + " Won!");
+            gameover.play();
+        }
+    });
+}
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', () => {
+        if (boxtext.innerText === '' && !isgameover) {
+            boxtext.innerText = turn;
+            element.classList.add('clicked');
+            setTimeout(() => element.classList.remove('clicked'), 300);
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            if (!isgameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+            }
+        }
+    });
+});
+
+document.getElementById('reset').addEventListener('click', resetGame);
+document.getElementById('playAgain').addEventListener('click', resetGame);
+document.getElementById('closeModal').addEventListener('click', closeModal);
+
+function resetGame() {
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = "";
+    });
+    turn = "X";
+    isgameover = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
+    closeModal();
+}
+
+function showModal(message) {
+    document.getElementById('gameoverMessage').innerText = message;
+    document.getElementById('gameoverModal').style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById('gameoverModal').style.display = "none";
+}
